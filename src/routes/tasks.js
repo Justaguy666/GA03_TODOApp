19339cf9fc55da68fe545abeb98acff87c6c76dc
KoinGame <obezet12@gmail.com>
2025-10-30 19:06:11 +0700

    Defines routes for actions

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,2 +1,20 @@
 const express = require('express');
-const rounter = express.Router();
+const router = express.Router();
+const taskController = require('../controllers/taskController');
+
+// Display all tasks
+router.get('/tasks', taskController.getAllTasks);
+
+// Add a new task
+router.post('/tasks', taskController.createTask);
+
+// Mark a task as completed
+router.put('/tasks/:id/complete', taskController.completeTask);
+
+// Unmark a task as not completed
+router.put('/tasks/:id/uncomplete', taskController.uncompleteTask);
+
+// Delete a task
+router.delete('/tasks/:id', taskController.deleteTask);
+
+module.exports = router;
