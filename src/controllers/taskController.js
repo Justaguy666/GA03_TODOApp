a322b0f29b58c17ae0b16500f4fed6aa24e68f49
KoinGame <obezet12@gmail.com>
2025-11-03 08:14:48 +0700

    .

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,19 +1,44 @@
 import taskStorage from '../models/taskStorage.js'
 
 class TaskController {
+    // Render the main page with tasks (Server-Side Rendering)
+    renderIndex = (req, res) => {
+        const tasks = taskStorage.getTasks();
+        res.render('index', { tasks });
+    }
+
+    // API endpoint to get tasks as JSON (for Client-Side Rendering)
     getTasks = (req, res) => {
         const tasks = taskStorage.getTasks();
         return res.status(200).json(tasks);
     }
 
+    // Add new task
+    addTask = (req, res) => {
+        const { description, priority, dueDate } = req.body;
+        if (description) {
+            taskStorage.add({ description, priority, dueDate });
+        }
+        res.redirect('/');
+    }
 
-    // markTask = (req, res) => {
-    //     const id = parseInt(req.params.id);
-    //     if (!isNaN(id)) {
-    //         taskStorage.mark(id);
-    //     }
-    //     res.redirect('/'); 
-    // }
+    // Mark task as completed
+    markTask = (req, res) => {
+        const id = parseInt(req.params.id);
+        if (!isNaN(id)) {
+            taskStorage.mark(id);
+        }
+        res.redirect('/');
+    }
+
+    // Delete task
+    deleteTask = (req, res) => {
+        const id = parseInt(req.params.id);
+        if (!isNaN(id)) {
+            taskStorage.delete(id);
+        }
+        res.redirect('/');
+    }
 }
 
 export default new TaskController();
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,40 @@ module.exports = {
     "./public/css/**/input.css"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          bg: '#2D2D2D',
+          card: '#3A3A3A',
+          item: '#3F3F3F',
+          border: '#444444',
+          text: '#EDEDED',
+          secondary: '#A8A8A8',
+        },
+        priority: {
+          high: '#E14B4B',
+          medium: '#D9A63A',
+          low: '#47B985',
+        },
+        btn: {
+          primary: '#5B46E5',
+          'primary-dark': '#32277F',
+          delete: '#FF5E5E',
+          edit: '#9E9E9E',
+        },
+        scroll: {
+          bg: '#EEEEEE',
+          thumb: '#777777',
+          arrow: '#555555',
+        }
+      },
+      fontFamily: {
+        'space': ['"Space Mono"', 'monospace'],
+      },
+      backdropBlur: {
+        '20': '20px',
+      },
+    },
   },
   plugins: [],
 }
